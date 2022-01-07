f03a729b813905c97144d178382e6ad31ce804c6
Vinícius Lucena <jviniiciuslucena@gmail.com>
2022-01-06 22:06:37 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    font-size: 50px;
+    color: ${({ theme }) => theme.colors.primary};
+`;
+
+const Home: NextPage = () => {
+    return <Title>My page</Title>;
+};
+
+export default Home;
