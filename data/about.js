268b21a1f2f627c1295b1b1ca4709d400849be01
Vinicius <jviniiciuslucena@gmail.com>
2022-10-20 10:40:50 -0300

    feat: add new job in about section

--- data/about.js
+++ data/about.js
@@ -1,6 +1,13 @@
 export const jobs = [
   {
     jobTitle: 'Front-end Developer',
+    company: 'Kunumi',
+    companyUrl: 'https://www.kunumi.com/',
+    startDate: '2022-10-24',
+    location: 'Belo Horizonte, Minas Gerais'
+  },
+  {
+    jobTitle: 'Front-end Developer',
     company: 'Innvo Digital',
     companyUrl: 'https://www.innvo.com.br/',
     startDate: '2021-12-08',
